850475482d0a57d2daae63bacaa7f3fabaa60d7d
shinon0821 <49701829+shinon0821@users.noreply.github.com>
2020-07-16 12:20:53 +0900

    変更

--- Assets/Hensu.cs
+++ Assets/Hensu.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-
+using System.Diagnostics;
 using UnityEngine;
 
 public class Hensu : MonoBehaviour
 {
+    int a = 10;
+
     // Start is called before the first frame update
-    void Start()
+    public void Proc()
     {
-        int a = 10;
+        
         float fw =  3.14f;
         string str = "こんにちは";
         bool bl = true;
@@ -17,6 +19,11 @@ public class Hensu : MonoBehaviour
         Debug.Log(fw);
         Debug.Log(str);
         Debug.Log(bl);
+    }
+
+    public void Proc2()
+    {
+        Debug.Log(a);
     }
 
     // Update is called once per frame
